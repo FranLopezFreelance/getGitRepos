04975842d0d254bb9c94b84ce5b421acf38e8901
frlopez <frlopez@WKS928L>
2020-05-10 22:08:18 -0300

    add paginator

--- scripts.js
+++ scripts.js
@@ -2,47 +2,60 @@
 let getReposFormElement = document.getElementById('get-repos-form');
 let searchingBoxElement = document.getElementById('searching-box');
 let userDataElement = document.getElementById('user-data');
-
 let resultsBoxElement = document.getElementById('results-box');
 let reposListElement = document.getElementById('repos-list');
 let errorBoxElement = document.getElementById('error-box');
-//Declare repos var 
+let errorMessageElement = document.getElementById('error-message');
+let paginatorElement = document.getElementById('paginator');
+let paginatorInfoElement = document.getElementById('paginator-info');
+let prevButtonElement = document.getElementById('prev-button');
+let nextButtonElement = document.getElementById('next-button');
+
+//Declare and initializate vars/CONST 
+let userName = '';
 let repos = [];
+let page = 1;
+const per_page = 10;
+let public_repos = 0;
+let pages = 1;
 
 //Submit Actions
 const submit = (e) => {
   e.preventDefault();
   searching();
-  let userName = getReposFormElement.elements['userName'].value;
+  resetVars();
+  //showPaginator(false);
+  userName = getReposFormElement.elements['userName'].value;
   if(validator(userName)){  
     getUser(userName)
       .then((userData) => {
         if(!userData.message){
-          getReposByUsername(userName)
+          public_repos = userData.public_repos
+          if(public_repos > 0){
+            calculatePages();
+            getRepos(userName, 1)
             .then(reposData => {
-              if(reposData.length){
-                injectReposData(reposData);
-                injectUserData(userData);
-                success();
-              }else{
-                injectUserData(userData);
-                injectNoReposTemplate();
-                success();
-              }
+              injectReposData(reposData);
+              injectUserData(userData);
+              success();
             })
             .catch((error) => {
-              alert(error)
+              error('Fetch data error');
             });
+          }else{
+            injectUserData(userData);
+            injectNoReposTemplate();
+            success();
+          }
         }else{
-          error();
+          error('Does not exist');
         }
       })
       .catch((error) => {
-        alert(error);
+        error('Fetch data error');
       });
-    
   }else{
-    error();
+    error('Invalid value');
   }
 }
 
@@ -53,13 +66,16 @@ const validator = (value) => {
   return (value.length > 0 && !(value.trim() == ""));
 }
 
+const clearRepos = () => {
+  reposListElement.innerHTML = '';
+}
+
 //Display Elements for searching
 const searching = () => {
   errorBoxElement.style.display = 'none';
   resultsBoxElement.style.display = 'none';
   searchingBoxElement.style.display = 'block';
-  //Clear repos list element
-  reposListElement.innerHTML = '';
+  clearRepos();
 }
 
 //Display Elements for success
@@ -70,9 +86,10 @@ const success = () => {
 }
 
 //Display Elements for error
-const error = () => {
+const error = (message) => {
   searchingBoxElement.style.display = 'none';
   resultsBoxElement.style.display = 'none';
+  errorMessageElement.innerText = message;
   errorBoxElement.style.display = 'block';
 }
 
@@ -83,9 +100,9 @@ const getUser = async (userName) => {
   return data;
 }
 
-//Get repos by user with fetch() api
-const getReposByUsername = async (userName) => {
-  let response = await fetch(`https://api.github.com/users/${userName}/repos`);
+//Get repos by userName, page with fetch() api 
+const getRepos = async (page) => {
+  let response = await fetch(`https://api.github.com/users/${userName}/repos?page=${page}&per_page=${per_page}`);
   let data = response.json();
   return data;
 }
@@ -112,6 +129,13 @@ const createRepoDataTemplate = (repo) => {
           </li>`;
 }
 
+//Create templates for paginator
+const createPaginatorTemplate = () => {
+  reposListElement.innerHTML = `<li class="repo">
+            <p class="no-repos">User has no repositories</p>
+          </li>`;
+}
+
 //Create templates for each repo with templates literal
 const injectNoReposTemplate = () => {
   reposListElement.innerHTML = `<li class="repo">
@@ -129,4 +153,66 @@ const injectReposData = (reposData) => {
   repos.map(repoData => {
     reposListElement.innerHTML += createRepoDataTemplate(repoData);
   });
+}
+
+const calculatePages = () => {
+  if(public_repos > per_page){
+    pages = Math.ceil(public_repos / per_page);
+    showPaginatorInfo();
+    showPaginator(true);
+  }else{
+    showPaginator(false);
+  }
+}
+
+const showPaginator = (value) => {
+  if(value){
+    paginatorElement.style.display = 'block';
+  }else{
+    paginatorElement.style.display = 'none';
+  }
+}
+
+const chagePage = (direction) => {
+  if(direction === 'prev'){
+    page--;
+  }else if(direction === 'next'){
+    page++;
+  }
+  checkPageToDisableButtons(page);
+  getRepos(page)
+    .then(reposData => {
+        clearRepos();
+        injectReposData(reposData);
+        showPaginatorInfo();
+        success();
+    })
+    .catch((error) => {
+      error('Fetch data error');
+    });
+}
+
+const checkPageToDisableButtons = (page) => {
+  if(page === 1){
+    prevButtonElement.disabled = true;
+    nextButtonElement.disabled = false;
+  }else if(page == pages){
+    prevButtonElement.disabled = false;
+    nextButtonElement.disabled = true;
+  }else{
+    prevButtonElement.disabled = false;
+    nextButtonElement.disabled = false;
+  }
+}
+
+const showPaginatorInfo = () => {
+  paginatorInfoElement.innerText = `${page} of ${pages}`;
+}
+
+const resetVars = () => {
+  userName = '';
+  repos = [];
+  page = 1;
+  public_repos = 0;
+  pages = 1;
 }
